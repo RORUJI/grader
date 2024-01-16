0d17cbf7f4964f300411b7a0cf52b91735b94612
RORUJI <asus.bg87@gmail.com>
2024-01-16 14:43:05 +0700

    index

--- index.php
+++ index.php
@@ -14,48 +14,61 @@ session_start();
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"/>
     <title>Grader</title>
 </head>
     
 <body class="">
     <div class="container-fluid">
         <div class="row" style="height: 100vh;">
-            <div class="col-2 bg-primary" >
-            <div class="row" style="height:10vh; width: auto; background:bisque;"></div>
+            <div class="col-2 sidebar" >
+            <div class="row" style="height:7vh; width: auto; background-color:rgb(193, 192, 192);"></div>
                 <a href="#" class="link-a"><i class="bi bi-house-fill"></i> Home</a>
                 <a href="#" class="link-a">Home</a>
                 <a href="#" class="link-a">Home</a>
                 <a href="#" class="link-a">Home</a>
             </div>
-            <div class="col bg-success">
-            <div class="row" style="height:10vh; width: auto; background:blueviolet;"></div> 
-                <div class="graph col bg-danger"></div>
+            <div class="col bannerbackground">
+            <div class="row" style="height:7vh; width: auto; background-color:rgb(193, 192, 192);">
+                <div class="d-flex justify-content-center"><input type="text" class="search" placeholder="&#xF002; Search" style="font-family:Arial, FontAwesome"></div>
+            </div> 
+                <div class="graph col banner"></div>
                 <div class="row">
-                    <a href="grader_question.php" class="graph1 col mx-3"><div class="div-1">SQL</div></a>
+                    <a href="SQL_question.php" class="graph1 col mx-3"><div class="div-1">SQL</div></a>
                     <a href="" class="graph2 col mx-3"><div class="div-2">ss</div></a>
                 </div>
             </div>
-            <div class="col-2 bg-danger">
-            <div class="row" style="height:10vh; width: auto; background:aquamarine;"></div>
+            <div class="col-2 sidebar">
+            <div class="row" style="height:7vh; width: auto; background-color:rgb(193, 192, 192);">
+                <div>
+                    <a href="" class="taskbar d-flex justify-content-center" style="height: 1.5vh;">
+                        <i class="taskbar1 bi bi-card-heading"></i>
+                        <i class="taskbar2 bi bi-bell"></i>
+                        <i class="taskbar3 bi bi-chat"></i>
+                        <i class="taskbar4 bi bi-gear"></i>
+                    </a>
+                </div>
+            </div>
                 <div class="container">
                     <div class="hexagon">
                         <div class="shape">
                             <img src="User.jpg" alt="">
                         </div>
-                        <p class="name">
-                            <?php if (isset($_SESSION['userID'])) : ?>
-                                <?php echo $_SESSION['username']; ?>
-                                <br/>
-                                <a href="system/logout_system.php" class="btn btn-danger">Logout</a>
-                            <?php endif; ?>
-                            <?php if (!isset($_SESSION['userID'])) : ?>
-                                Guest
-                                <br/>
-                                <a href="login.php" class="btn btn-primary">Login</a>
-                            <?php endif; ?>
-                        </p>
+                        
                     </div>
                 </div>
+                <p class="name">
+                    <?php if (isset($_SESSION['userID'])) : ?>
+                        <?php echo $_SESSION['username']; ?>
+                        <br/>
+                        <a href="system/logout_system.php" class="btn btn-danger">Logout</a>
+                    <?php endif; ?>
+                    <?php if (!isset($_SESSION['userID'])) : ?>
+                        Guest
+                        <br/>
+                        <a href="login.php" class="btn btn-secondary">Login</a>
+                    <?php endif; ?>
+                </p>
             </div>
         </div>
     </div>
